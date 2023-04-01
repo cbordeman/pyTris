10fb97a459cc8d6fc7e6821f83e4109088056fed
Chris Bordeman <cbordeman@outlook.com>
2023-04-01 18:23:10 -0400

    asdf

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-#from settings import *
+from settings import *
 import sys
+import pygame
 
 class App:
     def __init__(self):
-        pg.init()
+        pg.display.set_caption("PyTris!")
+        self.screen = pg.display.set_mode(FIELD_RES)
+        self.clock = pg.time.Clock()
+
+    def update(self):
+        self.clock.tick(FPS)
